73c2ad69661b5789cc6fe52a85e515dcd9de0669
Elisabeth Cami <ecami@cps.edu>
2025-10-29 10:57:19 -0500

    worked on solutions

--- a4.py
+++ a4.py
@@ -9,9 +9,24 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+    def __init__(self, board):
+        self.board = board['*'] * 9
+       
+    def __str__(self):
+        return f"{self.board[0]} {self.board [1]} {self.board[2]}\n{self.board[3]} {self.board [4]} {self.board[5]}\n{self.board[6]} {self.board [7]} {self.board[8]}\n"
+    
+    def make_move(self, player, pos):
+        if 0<= pos <= 8 and self.board[pos] == '*':
+            self.board[pos] = player
+            return True
+        return False
+    
+    def has_won(self, player):
+        pass 
 
-    pass
 
+    def game_over(self):
+        return self.has_won("X") or self.has_won("O") or '*' not in self.board
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
@@ -63,6 +78,7 @@ if __name__ == "__main__":
     brd = TTTBoard()
     brd.make_move("X", 8)
     brd.make_move("O", 7)
+    print(brd)
 
     assert brd.game_over() == False
 
